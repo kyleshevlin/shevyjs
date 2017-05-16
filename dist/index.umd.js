91aad9600622dd675cef1063e97106c2f0c30207
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-05-16 13:06:22 -0700

    run validate again

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["radhoc"] = factory();
+		exports["shevyjs"] = factory();
 	else
-		root["radhoc"] = factory();
+		root["shevyjs"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
